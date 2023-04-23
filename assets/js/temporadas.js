cc0771b642e34fb88258d76a9587719c5e8d9338
Marxsuel <charles.marx98@gmail.com>
2023-04-23 00:52:30 -0300

    Maratona Invertida Kenzie

---
+++ assets/js/temporadas.js
@@ -0,0 +1,26 @@
+let temporadas = [
+    {
+        id: 1,
+        nome: "Rick e Morty ",
+        sinopse: "Rick e Morty se envolvem em situações cada vez mais inesperadas, enquanto lidam com conflitos internos e externos",
+        imagemURL: "./src/assets/img/imagemCard.png",
+        imagemIcone: "./assets/img/icon2.png",
+        temporada: "Temporada 3"
+    },
+    {
+        id: 2,
+        nome: "Rick e Morty ",
+        sinopse: "A segunda temporada de Rick e Morty traz novas aventuras intergalácticas para a dupla disfuncional, incluindo viagens no tempo, dimensões alternativas e encontros com outras versões de si mesmos. Com um humor ácido e referências culturais, a série continua a desafiar as convenções do gênero sci-fi.",
+        imagemURL: "./src/assets/img/imagemCard.png",
+        imagemIcone: "./assets/img/icon2.png",
+        temporada: "Temporada 2"
+    },
+    {
+        id: 3,
+        nome: "Rick e Morty ",
+        sinopse: "A primeira temporada de Rick e Morty apresenta as aventuras absurdas e hilárias de um avô cientista maluco e seu neto desajeitado. Com episódios que exploram mundos paralelos, viagens no tempo e questões existenciais",
+        imagemURL: "./src/assets/img/imagemCard.png",
+        imagemIcone: "./assets/img/icon2.png",
+        temporada: "Temporada 1"
+    },
+]
